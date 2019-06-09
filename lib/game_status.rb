ee50e0ab1e4dd99a154233f63ca0b81fac42f912
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-06-09 18:51:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-def won?
+def won
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   win_combos = [
   [0,1,2], # Top row
